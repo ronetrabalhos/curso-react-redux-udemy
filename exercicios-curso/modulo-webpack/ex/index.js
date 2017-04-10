8c196b89b739e25a291d9404792765d7758ae297
rone.trabalhos <rone.trabalhos@gmail.com>
2017-04-10 12:54:48 -0300

    Alteração em comentário

--- exercicios-curso/modulo-webpack/ex/index.js
+++ exercicios-curso/modulo-webpack/ex/index.js
@@ -4,7 +4,7 @@ console.log('Webpack iniciado com sucesso!');
 logger.js 
 
 Quando se tratar dos meus arquivos, referencio com caminho relativo ./
-Sendo do node_modules basta inserir o nome
+Sendo do node_modules basta inserir o nome  :)
 */
 const logger = require( './logger' );
 logger.info("Usando o padrão CommonsJS");
