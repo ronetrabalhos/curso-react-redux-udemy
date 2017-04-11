00bb35a3a1176ab874788e1589c8fb379766d4d4
rone.trabalhos <rone.trabalhos@gmail.com>
2017-04-11 14:04:15 -0300

    aula 12 - Instalacao e configuracao da dependencia Babel para interpretacao do import e export

--- exercicios-curso/modulo-webpack/webpack.config.js
+++ exercicios-curso/modulo-webpack/webpack.config.js
@@ -20,5 +20,17 @@ module.exports = {
         port: 8080,                     /* Porta */
         contentBase: './public'         /* Pasta onde está os arquivos, inclusive o bundle.js e o index.html*/
 
+    },
+
+    module: {
+        loaders: [{                     /* loaders são responsáveis por carregar os arquivos que resolverão nosso problemas, como o Babel*/
+            test: /.js?$/,              /* tipo de arquivo que será carregado */
+            loader: 'babel-loader',       /* qual loader iremos utilizar para a tarefa. Essa dependência deve instalada e referenciada no arquivo package.json */
+            exclude: /node_module/,     /* queremos apenas que o Babel leia os js de nossa aplicação, por isso retiramos a pastanode_module  */
+            query: {                    /* dentro de query definimos o que será interpretado pelo Babel interprete */
+                presets: ['es2015']
+            }
+        }]
     }
+
 }
