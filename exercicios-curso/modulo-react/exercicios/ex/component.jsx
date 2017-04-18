d0c558e56008598a293cd846bf385c5680e3b432
Rone Menezes <rone.trabalhos@gmail.com>
2017-04-18 13:26:45 -0300

    Aula 21 - Arrow Function (maneira compacta para escrever funções)

--- exercicios-curso/modulo-react/exercicios/ex/component.jsx
+++ exercicios-curso/modulo-react/exercicios/ex/component.jsx
@@ -4,8 +4,20 @@ import React from 'react'
 //Por ser construído em um arquivo separado de onde será chamado
 //é necessário exporta-lo (deixa-lo visivel para outros arquivos)
 
-export default function(){
-    console.log('[component.js] - retornando o primeiro componente')
-    //O retorno será um componente react
-    return <h1> Meu primeiro componente! </h1>
-}
+// ARROW FUNCTION
+// Substitui a palavra function pelo sinal => 
+// Remover os { } e o return e colocar tudo em uma única linha, 
+// pois o return está implicito em uma Arrow Function
+// Como nossas funções possuem mais uma linha, devemos tratá-las
+// como expressão. Em resumo: tudo que estiver dentro de ( ) será 
+// retornado na tela
+
+export default () => ( 
+    <h1> Componente utilizando Arrow Function! </h1>
+)
+    
+
+// NOTA    
+// Não consegui utilizar console.log com arrow function
+// console.log('[component.js] - retornando o primeiro componente')
+    
