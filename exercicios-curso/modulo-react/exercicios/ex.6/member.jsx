6b0519cd57e43964509f4a325710fc30e2e952f2
Rone Menezes <rone.trabalhos@gmail.com>
2017-04-18 15:26:47 -0300

    Aula 24 - Arvore de componentes (comp. filhos)

---
+++ exercicios-curso/modulo-react/exercicios/ex.6/member.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// Parâmetros de entrada: name e lastName
+
+export default props => (
+    <div>
+        {props.name} <strong> {props.lastName} </strong>
+    </div>
+)
