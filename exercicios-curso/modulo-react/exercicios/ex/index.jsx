7c45745b9198862deb4e6d9e0a165a74b68ae632
Rone Menezes <rone.trabalhos@gmail.com>
2017-04-25 15:05:32 -0300

    - Aula 28: Componente baseado em classe

--- exercicios-curso/modulo-react/exercicios/ex/index.jsx
+++ exercicios-curso/modulo-react/exercicios/ex/index.jsx
@@ -1,31 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import Family from './family'
-import Family from './family.childrenWithProps'
-import Member from './member'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
-    <Family lastName = 'da Silva'> 
-
-        <Member name = 'Gustavo' />
-        <Member name = 'Caixxxinhos' />
-        <Member name = 'Flávia' />
-        <Member name = 'Ana' />
-        <Member name = 'Paula' />
-
-    </Family>
+    <ClassComponent value='Componente Classe' />
     , document.getElementById('app')
 )
-
-
-/*
-ReactDOM.render(
-    <Family lastName = 'dos Santos'> 
-
-        <Member name = 'Neymar' />
-        <Member name = 'Bruna' />
-
-    </Family>
-    , document.getElementById('app')
-)
-*/
--- exercicios-curso/modulo-react/exercicios/ex/member.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// Parâmetros de entrada: name e lastName
-
-export default props => (
-    <div>
-        {props.name} <strong> {props.lastName} </strong>
-    </div>
-)
