b765e83f56fd19b408f5c6d8ead7a456754a36da
Rone Menezes <rone.trabalhos@gmail.com>
2017-04-26 10:42:22 -0300

    - Aula 29: Criando um componente mais complexo, baseado em classe, com chamada de função, state e setState

--- exercicios-curso/modulo-react/exercicios/ex/classComponent.jsx
+++ exercicios-curso/modulo-react/exercicios/ex/classComponent.jsx
@@ -20,9 +20,50 @@ import React, { Component } from 'react'
 
 export default class ClassComponent extends Component {
 
+    // Para manipular variáveis criaremos o construtor da classe
+    // passando props e adiconaremos ao construtor o método super() 
+    // passando props. O super() é obrigadtório 
+
+    constructor(props){
+        
+        // Método obrigatório
+        super(props)
+
+        // Variável state é recebida por herança
+        // Todo componente possui essa variável 
+        // A ação abaixo inicializa o componente
+        // atribuindo um valor passado por props
+        // através da variável initialValue
+        this.state = { value: props.initialValue }
+    }
+
+
+    // método para incrementar e decrementar o valor que está em state
+    sum(delta){
+
+        this.setState({value: this.state.value + delta })
+
+    }
+
+
     render(){
+        // NOTA IMPORTANTE: observe que o componente está envolto em uma
+        // div. Isso é necessário pois, só é possível retornar apenas 
+        // um componente por vez. Portanto, neste caso estamos retornando
+        // uma div com toda estrutura do componente.
+
+        // Para chamar a função sum() em onClick, temos que utilizar
+        // Arrow Function, pois senão estaremos atribuindo um valor 
+        // indefinido a uma valor inteiro, como é esperado.
         return (
-            <h1> { this.props.value } </h1>
+            <div>
+                <h1> { this.props.label } </h1>
+                <h2> { this.state.value } </h2>
+
+                <button onClick={ () => this.sum(1)}> Incrementar </button>
+                <button onClick={ () => this.sum(-1)}> Decrementar </button>
+                
+            </div>
         )
     }
 
--- exercicios-curso/modulo-react/exercicios/ex/index.jsx
+++ exercicios-curso/modulo-react/exercicios/ex/index.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ClassComponent from './classComponent'
 
+// Passando inteiro ao invés de string: coloca entre chaves
+// Trabalhando com um componente um pouco mais robusto, 
+// definimos na classComponent, uma função que receberá 
+// dois valores: label e inicialValue
+// Eles serão utilizamos na composição do componente.
+// Vale recordar que qualquer valor recebido por props é
+// somente leitura. Não pode ser manipulado.
+
+// Para manipular implementamos o construtor e o inicializamos
+// com o valor da variável inicialValue
+ 
 ReactDOM.render(
-    <ClassComponent value='Componente Classe' />
+    <ClassComponent label='Contador' initialValue={10} />
     , document.getElementById('app')
 )
