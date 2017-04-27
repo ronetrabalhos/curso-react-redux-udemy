2dad52796bf2225abf077404a8b637826e6ddc4f
Rone Menezes <rone.trabalhos@gmail.com>
2017-04-27 15:30:53 -0300

    - Aula 36: Mapeamento de Rotas [ IMPORTANTE ]

--- exercicios-curso/todo-app/backend/src/api/todo/todo.js
+++ exercicios-curso/todo-app/backend/src/api/todo/todo.js
@@ -1,7 +1,7 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const todoSchema = new mongoose.todoSchema({
+const todoSchema = new mongoose.Schema({
 
     description : { type: String, required: true },
     done        : { type: Boolean, required: true, default: false },
