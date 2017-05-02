7435ccc35d0f4006162420f3cbad90d621aa9d79
Rone Menezes <rone.trabalhos@gmail.com>
2017-05-02 14:41:01 -0300

    - Aula 44: Componentes ToDo e About

--- exercicios-curso/todo-app/frontend/src/main/app.jsx
+++ exercicios-curso/todo-app/frontend/src/main/app.jsx
@@ -2,6 +2,8 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 // Componente baseado em função arrow
 // O uso do parênteses (  ) é pq será retornado uma expressão
@@ -9,7 +11,9 @@ import React from 'react'
 // for utilizado as chaves {  }
 
 export default props => (
+    
     <div className='container'>
-        <h1>Teste</h1>
+        <Todo />
+        <About />
     </div>
 )
---
+++ exercicios-curso/todo-app/frontend/src/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component{
+
+    render(){
+        return (
+            <div>
+                <h1> ToDo Component </h1>
+            </div>
+        )
+    }
+}
