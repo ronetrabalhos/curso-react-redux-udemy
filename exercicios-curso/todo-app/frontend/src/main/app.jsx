ce2270a3f6eb6ed0543c0b14b13b42d38f800193
Rone Menezes <rone.trabalhos@gmail.com>
2017-05-02 13:07:16 -0300

    - Aula 43: Criação e configuração dos arquivos index.jsx e app.jsx, como também da estrutura de pastas src e src/main

---
+++ exercicios-curso/todo-app/frontend/src/main/app.jsx
@@ -0,0 +1,15 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+// Componente baseado em função arrow
+// O uso do parênteses (  ) é pq será retornado uma expressão
+// Em uma função arrow o return está implicito, exceto se
+// for utilizado as chaves {  }
+
+export default props => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)
