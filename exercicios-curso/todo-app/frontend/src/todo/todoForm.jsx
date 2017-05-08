4c0127c7923525880e0dd2105ec3ca3ad83ae624
Rone Menezes <rone.trabalhos@gmail.com>
2017-05-08 13:58:49 -0300

    - Aula 60: Inserindo teclas de atalho

--- exercicios-curso/todo-app/frontend/src/todo/todoForm.jsx
+++ exercicios-curso/todo-app/frontend/src/todo/todoForm.jsx
@@ -3,40 +3,54 @@ import Grid  from '../template/grid'
 import IconButton  from '../template/iconButton'
 
 
-export default props => (
+export default props => {
 
-    <div role='form' className='todoForm'>  
+    // Esta função adiciona teclas de atalho ao formulário
+    const keyHandler = (e) => {
+        if( e.key === 'Enter' ){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }
+        else if ( e.key === 'Escape' ){
+            props.handleClear()
+        }
+    }
 
-       <Grid cols='12 9 10'> 
-            <input id='description' 
-                   className = 'form-control'
-                   placeholder = 'Adicione uma tarefa'
-                   onChange = { props.handleChange }
-                   value = { props.description } >
-            </input>
-       </Grid>
 
-       <Grid cols='12 3 2'> 
-           <IconButton 
-                style='primary' 
-                icon='plus' 
-                onClick = { props.handleAdd } > 
-            </IconButton>         
+    return (
 
-           <IconButton 
-                style='info' 
-                icon='search' 
-                onClick = { props.handleSearch } > 
-            </IconButton>  
+        <div role='form' className='todoForm'>  
 
-           <IconButton 
-                style='default' 
-                icon='close' 
-                onClick = { props.handleClear } > 
-            </IconButton>  
+            <Grid cols='12 9 10'> 
+                    <input id='description' 
+                        className = 'form-control'
+                        placeholder = 'Adicione uma tarefa'
+                        onChange = { props.handleChange }
+                        onKeyUp = { keyHandler }
+                        value = { props.description } >
+                    </input>
+            </Grid>
 
-       </Grid>
+            <Grid cols='12 3 2'> 
+                <IconButton 
+                        style='primary' 
+                        icon='plus' 
+                        onClick = { props.handleAdd } > 
+                    </IconButton>         
 
-    </div>
+                <IconButton 
+                        style='info' 
+                        icon='search' 
+                        onClick = { props.handleSearch } > 
+                    </IconButton>  
 
-)
+                <IconButton 
+                        style='default' 
+                        icon='close' 
+                        onClick = { props.handleClear } > 
+                    </IconButton>  
+
+            </Grid>
+        </div>
+    )
+
+}
