6f8370a699ef7f7a530ba31e9520b71ede317796
Rone Menezes <rone.trabalhos@gmail.com>
2017-05-09 13:17:36 -0300

    - Organização de arquivos para início dos trabalhos. A pasta [ex] = [ex10]

--- exercicios-curso/modulo-rr-react-redux/exercicios/ex.12/index.jsx
+++ exercicios-curso/modulo-rr-react-redux/exercicios/ex.12/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
-
+import Field from './field'
+ 
 ReactDOM.render(
-    <ClassComponent value='Componente Classe' />
+    <Field inicialValue='Teste' />
     , document.getElementById('app')
 )
