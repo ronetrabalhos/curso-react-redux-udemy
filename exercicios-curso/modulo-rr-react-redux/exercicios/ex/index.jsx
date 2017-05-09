249bf2584616272c37a5140f78af4499baf5a084
Rone Menezes <rone.trabalhos@gmail.com>
2017-05-09 15:03:28 -0300

    - Aula 63: Configurando o Redux (importante)

--- exercicios-curso/modulo-rr-react-redux/exercicios/ex/index.jsx
+++ exercicios-curso/modulo-rr-react-redux/exercicios/ex/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import Field from './field'
+
+/*
+field é um parâmetro e será atribuído a ele uma [função reducers],
+logo o resultado do field será uma função (pode ser uma função
+Arrow () => (...) ). 
+
+value é um atributo da função arrow que será retornada
+
+NOTA:
+Reducers são funções puras. Recebem o estado original, recebe a ação
+que aconteceu. Em cima dessas duas informações ele vai gerar uma versão
+atualizada do estado
+
+*/
+const reducers = combineReducers({
+    field: () => ( { value: 'opa' } )
+})
  
 ReactDOM.render(
-    <Field inicialValue='Teste' />
+    <Provider store = { createStore(reducers) } >
+        <Field inicialValue='Teste' />
+    </Provider>
     , document.getElementById('app')
 )
