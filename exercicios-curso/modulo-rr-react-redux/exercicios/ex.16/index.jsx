6cdc78f236fdf6ac892dc8867009211d8a762855
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-05-15 10:21:17 -0300

    - Aula 66: BKP

---
+++ exercicios-curso/modulo-rr-react-redux/exercicios/ex.16/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import Field from './field'
+import fieldReducer from './fieldReducer'
+
+/*
+field é um parâmetro e será atribuído a ele uma [função reducers],
+logo o resultado do field será uma função (pode ser uma função
+Arrow () => (...) ). 
+
+value é um atributo da função arrow que será retornada
+
+NOTA:
+Reducers são funções puras. Recebem o estado original, recebe a ação
+que aconteceu. Em cima dessas duas informações ele vai gerar uma versão
+atualizada do estado
+
+*/
+const reducers = combineReducers({
+    //field: () => ( { value: 'opa' } )
+    field: fieldReducer
+})
+ 
+ReactDOM.render(
+    <Provider store = { createStore(reducers) } >
+        <Field inicialValue='Teste' />
+    </Provider>
+    , document.getElementById('app')
+)
