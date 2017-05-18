83df58af819a554ea5307757408e1f8dbbaec5bf
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-05-18 13:38:39 -0300

    - Aula 75: Instalação e configurando o Redux DevTools

--- exercicios-curso/todo-app-rr/frontend/src/index.jsx
+++ exercicios-curso/todo-app-rr/frontend/src/index.jsx
@@ -6,7 +6,11 @@ import { Provider } from 'react-redux'
 import App from './main/app'
 import reducers from './main/reducers'
 
-const store = createStore(reducers)
+// Instanciando o DevTools
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+                 && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducers, devTools)
 
 ReactDOM.render(
     <Provider store = { store } >
