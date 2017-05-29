00eb2560cfc49466fe024405a414fd8edbb5d3e9
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-05-29 13:40:17 -0300

    - Aula 93: [MM] Criação do método / rota para contar número de registros. Utilizado para a paginação

--- exercicios-curso/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ exercicios-curso/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,18 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+// Criação da rota 'count' que conterá a quantidade de registros
+// persistentes no banco de dados
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error){
+            res.status(500).json({ errors : [error] })
+        }
+        else {
+            res.json({ value })
+        }
+    })
+})
+
+
 module.exports = BillingCycle
