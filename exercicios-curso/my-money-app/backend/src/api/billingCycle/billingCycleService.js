6e25ec398cdf93766407d88324eae94ae0417df6
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-05-30 10:32:43 -0300

    - Aula 94: [MM] Obtendo o Sumário de Pagamentos

--- exercicios-curso/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ exercicios-curso/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -5,6 +5,7 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+
 // Criação da rota 'count' que conterá a quantidade de registros
 // persistentes no banco de dados
 BillingCycle.route('count', (req, res, next) => {
@@ -16,6 +17,29 @@ BillingCycle.route('count', (req, res, next) => {
             res.json({ value })
         }
     })
+})
+
+// Criação da rota / serviço para retornar o sumário de todos 
+// os ciclos de pagamento.
+// Valor consolidado de tudo que foi cadastrado no bd
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate({
+        $project: { credit : {$sum: "$credits.value"}, 
+                    debt  : {$sum: "$debts.value"} }
+    }, {
+        $group: { _id : null, 
+                credit: {$sum: "$credit"}, 
+                debt : {$sum: "$debt"}  }
+    }, {
+        $project: { _id: 0, credit: 1, debt: 1}
+    }, (error, result) => {
+        if(error){
+            res.status(500).json({ errors : [error] })
+        }
+        else {
+            res.json( result[0] || {credit: 0, debt: 0} )
+        }
+    } )
 })
 
 
