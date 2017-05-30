3cac30e9688d058ae931b8d12adc213252ed3ddc
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-05-30 15:13:55 -0300

    - Aula 97: [MM] Middleware Query Parser Int (correção para a paginação)

--- exercicios-curso/my-money-app/backend/src/config/server.js
+++ exercicios-curso/my-money-app/backend/src/config/server.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParse = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParse())
 
 server.listen(port, function(){
     console.log(`[ MY-MONEY-APP ] [ BACKEND ] Rodando na porta ${port}.`)
