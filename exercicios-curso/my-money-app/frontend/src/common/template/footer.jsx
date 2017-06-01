726c3e18994e948b3f1f2e26ae1b4dde58c0f5af
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-06-01 15:33:43 -0300

    - Aula 107: [MM] - Componente Rodapé

---
+++ exercicios-curso/my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017 - 
+            <a href='http://apple.com.br' target='_blank'> Apple</a>
+        </strong>
+    </footer>
+)
--- exercicios-curso/my-money-app/frontend/src/main/app.jsx
+++ exercicios-curso/my-money-app/frontend/src/main/app.jsx
@@ -5,6 +5,8 @@ import React from 'react'
 // Componentes que compõem o template
 import Header  from '../common/template/header'
 import Sidebar from '../common/template/sideBar'
+import Footer  from '../common/template/footer'
+
 
 // Tudo o que for exportado a partir de props será o template
 // que será criado a partir deste componente
@@ -13,5 +15,9 @@ export default props => (
     <div className='wrapper'>
         <Header /> 
         <Sidebar />
+        <div className='content-wrapper'>
+            <h1>Conteúdo [MM] </h1>
+        </div>
+        <Footer />
     </div>
 )
