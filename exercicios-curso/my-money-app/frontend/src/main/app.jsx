4ceac20159afc9fe3cfaa2e2dfa8505e354f8887
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-06-01 13:20:49 -0300

    - Aula 105: [MM] - Componente Menu (Parte 1)

--- exercicios-curso/my-money-app/frontend/src/main/app.jsx
+++ exercicios-curso/my-money-app/frontend/src/main/app.jsx
@@ -3,7 +3,8 @@ import '../common/template/dependencies'
 import React from 'react'
 
 // Componentes que compõem o template
-import Header from '../common/template/header'
+import Header  from '../common/template/header'
+import Sidebar from '../common/template/sideBar'
 
 // Tudo o que for exportado a partir de props será o template
 // que será criado a partir deste componente
@@ -11,5 +12,6 @@ import Header from '../common/template/header'
 export default props => (
     <div className='wrapper'>
         <Header /> 
+        <Sidebar />
     </div>
 )
