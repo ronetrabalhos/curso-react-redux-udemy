a186073d064d5039ec2ddf1a2ed5b1cad98a665c
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-06-01 09:55:35 -0300

    - Aula 102 [MM] - Componentes Index.jsx e App.jsx

---
+++ exercicios-curso/my-money-app/frontend/src/index.jsx
@@ -0,0 +1,8 @@
+// Este arquivo é um componente react
+// ReactDOM interagir com a página
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+// O nome do getElementById foi definido no arquivo app.jsx
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ exercicios-curso/my-money-app/frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+// Este arquivo é um componente react
+import React from 'react'
+
+// Tudo o que for exportado a partir de props será o template
+// que será criado a partir deste componente
+export default props => (
+    <div className='wrapper'>
+        <h1>[MM] - app.jsx</h1>
+    </div>
+)
