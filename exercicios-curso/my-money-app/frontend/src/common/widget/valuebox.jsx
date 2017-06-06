a15b1a28aae34f2b5ce6244b6092c14c920e2d4c
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-06-06 14:17:30 -0300

    - [MM] - indentação do código do componente valueBox

--- exercicios-curso/my-money-app/frontend/src/common/widget/valuebox.jsx
+++ exercicios-curso/my-money-app/frontend/src/common/widget/valuebox.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import Grid from '../layout/grid'
 
 export default props => (
-        <Grid cols={props.cols}>
-            
-            <div className={`small-box bg-${props.color}`}>
-                <div className='inner'>
-                    <h3>{props.value}</h3>
-                    <p>{props.text}</p>
-                </div>
-                <div className='icon'>
-                    <i className={`fa fa-${props.icon}`}></i>
-                </div>
+    <Grid cols={props.cols}>
+        
+        <div className={`small-box bg-${props.color}`}>
+            <div className='inner'>
+                <h3>{props.value}</h3>
+                <p>{props.text}</p>
             </div>
+            <div className='icon'>
+                <i className={`fa fa-${props.icon}`}></i>
+            </div>
+        </div>
 
-        </Grid>
+    </Grid>
 )
