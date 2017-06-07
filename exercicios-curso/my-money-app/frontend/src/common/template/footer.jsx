970641ea09dd1ff613e21363863909da853ef636
Inforlube - Estudos <rone.trabalhos@gmail.com>
2017-06-07 11:23:27 -0300

    - [MM] - Correção de layout

--- exercicios-curso/my-money-app/frontend/src/common/template/footer.jsx
+++ exercicios-curso/my-money-app/frontend/src/common/template/footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default props => (
     <footer className='main-footer'>
+        <div className='pull-right'>
+            <b>teste</b>
+        </div>
+
         <strong>
             Copyright &copy; 2017 - 
             <a href='http://apple.com.br' target='_blank'> Apple</a>
